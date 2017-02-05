ee08f983a2a4e36f247d559e323964c68957ff07
Naveen Tiwari <naveen.k.tiwari@gmail.com>
2017-02-04 18:41:41 -0700

    Changed the layout of the website.
    Currently I am using bootstrap.min.css

---
+++ resource/helper_script.js
@@ -0,0 +1,13 @@
+
+
+function writetab() {
+    document.write(String.fromCharCode(160, 160, 160, 160));
+}
+
+function writePersonalMail() {
+    document.write(String.fromCharCode(110, 97, 118, 101, 101, 110, 46, 107, 46, 116, 105, 119, 97, 114, 105, 64, 103, 109, 97, 105, 108, 46, 99, 111, 109));
+}
+
+function writeASUMail() {
+    document.write(String.fromCharCode(110, 97, 118, 101, 101, 110, 46, 116, 105, 119, 97, 114, 105, 64, 97, 115, 117, 46, 101, 100, 117));
+}
